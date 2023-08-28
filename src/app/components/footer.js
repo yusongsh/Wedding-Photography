f746cf455b1ea11fbb64fbe3339f065eb3a2a3de
Yusong Shi <jackshi.sys@gmail.com>
2023-08-27 21:03:52 -0400

    nav/footer

---
+++ src/app/components/footer.js
@@ -0,0 +1,151 @@
+"use client";
+import * as React from "react";
+import Link from "next/link";
+import Logo from "../images/YusongShiWeddingLogo.png";
+import Image from "next/image";
+import { useState, useEffect } from "react";
+
+const query = `
+{
+  igPostCollection {
+    items{
+     slug
+     title
+     photosCollection{
+       items {
+         url
+       }
+     }
+   }
+     }
+ 
+}
+`;
+
+export default function Footer() {
+  const [page, setPage] = useState(null);
+
+  useEffect(() => {
+    window
+      .fetch(
+        `https://graphql.contentful.com/content/v1/spaces/${process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID}/`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN}`,
+          },
+          body: JSON.stringify({ query }),
+        }
+      )
+      .then((response) => response.json())
+      .then(({ data, errors }) => {
+        if (errors) {
+          console.error(errors);
+        }
+
+        setPage(data);
+      });
+  }, []);
+
+  if (!page) {
+    return (
+      <div className="w-full h-screen flex justify-center items-center">
+        <span>Loading...</span>
+      </div>
+    );
+  }
+
+  const igPost = page.igPostCollection.items[0].photosCollection.items;
+
+  console.log("can you see", page, igPost);
+
+  return (
+    <footer>
+      <div className="p-[6vw] lg:py-[2vw] lg:px-[2vw] w-full flex flex-col gap-16 lg:gap-10">
+        <div className="hidden md:block py-8">
+          <div className="flex overflow-hidden gap-2 w-full">
+            {igPost.map((image, index) => (
+              <div key={index} className="w-full h-auto aspect-square relative">
+                <img
+                  src={image.url}
+                  alt="ig post for Yusong Wedding"
+                  loading="lazy"
+                  className="w-full h-auto object-cover aspect-square relative"
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div className="w-full md:hidden">
+          <div className="grid grid-cols-2 gap-2">
+            {igPost.map((image, index) => (
+              <div key={index} className="w-full h-auto aspect-square relative">
+                <img
+                  src={image.url}
+                  alt="ig post for Yusong Wedding"
+                  loading="lazy"
+                  className="w-full h-auto object-cover aspect-square relative"
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+
+        {/* bottom section with link and logo */}
+        <div className="flex flex-col md:items-end md:flex-row justify-between text-light gap-10">
+          <div className="pb-6 md:pb-0">
+            <div className="uppercase font-semibold text-lg md:text-base flex flex-col lg:flex-row gap-2 md:gap-1">
+              <Link href="/" className="">
+                Home
+              </Link>
+              <span className="hidden lg:block">-</span>
+              <Link href="/portfolio" className="">
+                Portfolio
+              </Link>
+              <span className="hidden lg:block">-</span>
+              <Link href="/blog" className="">
+                Blog
+              </Link>
+              <span className="hidden lg:block">-</span>
+              <Link href="/faqs" className="">
+                FAQs
+              </Link>
+              <span className="hidden lg:block">-</span>
+              <Link href="/about" className="">
+                About
+              </Link>
+              <span className="hidden lg:block">-</span>
+              <Link href="/inquire" className="">
+                Inquire
+              </Link>
+            </div>
+            <div className="text-xs pt-2">
+              <a>Brand Credit</a>
+              <span> | </span>
+              <a>Site Credit</a>
+            </div>
+          </div>
+          <div className="flex flex-col md:items-end gap-2 lg:gap-0">
+            <div className="w-36 h-auto aspect-[4/1] relative">
+              <Link href="/" className="text-2xl font-bold">
+                <Image
+                  src={Logo}
+                  alt="Site Logo"
+                  fill={true}
+                  style={{ objectFit: "contain" }}
+                />
+              </Link>
+            </div>
+            <div>
+              <p className="text-xs pr-1">
+                @YUSONG SHI PHOTOGRAPHY 2020-PRESENT
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
---
+++ src/app/components/nav.js
@@ -0,0 +1,147 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
+import { FaTwitter, FaInstagram } from "react-icons/fa";
+import { motion, useAnimation } from "framer-motion";
+import Logo from "../images/YusongShiWeddingLogo.png";
+import Image from "next/image";
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [visible, setVisible] = useState(true);
+  const controls = useAnimation();
+
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "unset";
+    }
+  }, [isOpen]);
+
+  const line1Variants = {
+    open: { rotate: 45, translateY: 5 },
+    closed: { rotate: 0, translateY: 0 },
+  };
+
+  const line2Variants = {
+    open: { rotate: -45, translateY: -5 },
+    closed: { rotate: 0, translateY: 0 },
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.scrollY;
+      setVisible(prevScrollPos > currentScrollPos || currentScrollPos < 10);
+      setPrevScrollPos(currentScrollPos);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [prevScrollPos]);
+
+  useEffect(() => {
+    if (visible) {
+      controls.start({
+        y: 0,
+        transition: { type: "spring", stiffness: 260, damping: 20 },
+      });
+    } else {
+      controls.start({
+        y: "-100%",
+        transition: { type: "spring", stiffness: 260, damping: 20 },
+      });
+    }
+  }, [visible, controls]);
+
+  return (
+    <motion.div animate={controls} className="px-8 py-4">
+      <nav className="sticky top-0 w-full text-white z-50 bg-transparent">
+        <div className="flex items-center justify-between">
+          <div className="w-36 h-auto aspect-[4/1] relative">
+            <Link href="/" className="text-2xl font-bold">
+              <Image
+                src={Logo}
+                alt="Site Logo"
+                fill={true}
+                style={{ objectFit: "contain" }}
+              />
+            </Link>
+          </div>
+          <button
+            onClick={() => setIsOpen(!isOpen)}
+            className="text-white focus:outline-none"
+          >
+            <svg width="30" height="20">
+              <motion.line
+                x1="0"
+                y1="5"
+                x2="30"
+                y2="5"
+                stroke="#D2C69E"
+                strokeWidth="1"
+                variants={line1Variants}
+                initial={false}
+                animate={isOpen ? "open" : "closed"}
+              />
+              <motion.line
+                x1="0"
+                y1="15"
+                x2="30"
+                y2="15"
+                stroke="#D2C69E"
+                strokeWidth="1"
+                variants={line2Variants}
+                initial={false}
+                animate={isOpen ? "open" : "closed"}
+              />
+            </svg>
+          </button>
+        </div>
+      </nav>
+      {isOpen && (
+        <div className="fixed inset-0 bg-black flex flex-col items-center justify-center text-center text-light pt-16 z-30">
+          <Link
+            href="/"
+            className="block py-1 text-5xl uppercase font-semibold"
+            onClick={() => setIsOpen(false)}
+          >
+            Home
+          </Link>
+          <Link
+            href="/about"
+            className="block mt-6 py-1 text-5xl uppercase font-semibold"
+            onClick={() => setIsOpen(false)}
+          >
+            About
+          </Link>
+          <Link
+            href="/contact"
+            className="block mt-6 py-1 text-5xl uppercase font-semibold"
+            onClick={() => setIsOpen(false)}
+          >
+            Contact
+          </Link>
+          <div className="fixed bottom-0 left-0 right-0 flex items-center justify-center pb-4">
+            <a
+              href="https://www.twitter.com"
+              className="mx-2 text-sm text-light"
+            >
+              <FaTwitter />
+            </a>
+            <a
+              href="https://www.instagram.com"
+              className="mx-2 text-sm text-light"
+            >
+              <FaInstagram />
+            </a>
+          </div>
+        </div>
+      )}
+    </motion.div>
+  );
+};
+
+export default Navbar;
